a70050ce7c5bff54d2aad382d3db2e97c5e4478f
thiagocastillo <thiago.castillo.cbt@gmail.com>
2026-09-16 19:26:18 -0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,28 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import CountriesPage from "./pages/CountriesPage.jsx";
 import CountrieDetailPage from "./pages/CountrieDetailPage.jsx";
-import {ThemeProvider} from "./context/ThemeContext.jsx";
+import { ThemeProvider, useTheme } from "./context/ThemeContext.jsx";
+
 function App() {
   return (
-    <>
-      <ThemeProvider>
-          <Header />
-          {/* 👇 Here you can use Routes or Switch component, Routes match with the most
-      specific route so the order doesn't matter  */}
-          <Routes>
-              {/* you can use 👇 character (* or wildcard) to any other route
-        in combination with the Navigate component you can redirect the user to a default page */}
-              <Route path="/*" element={<Navigate replace to="/countries" />} />
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  );
+}
 
-              {/* You can compose the routes by wrapping the Route components  and using
-        the Outlet Component from React Router*/}
-              <Route path="/countries" element={<CountriesPage />}></Route>
+function AppContent() {
+  const { darkMode } = useTheme();
 
-              {/* If composing routes doesn't looks clear for you, you always can define
-        the whole 👇route and expect the router will match the most specific one */}
-              <Route path="/countries/:countrieId" element={<CountrieDetailPage />} />
-          </Routes>
-      </ThemeProvider>
-
-    </>
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <Header />
+      <Routes>
+        <Route path="/*" element={<Navigate replace to="/countries" />} />
+        <Route path="/countries" element={<CountriesPage />}></Route>
+        <Route path="/countries/:countrieId" element={<CountrieDetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
